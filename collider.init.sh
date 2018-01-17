25c74cccd7d862ad24ddd0ed8b749b8b8a2439e0
Oleksandr <ol.yuvchenko@gmail.com>
2018-01-17 08:36:47 +0200

    Added Collider Start Function

--- collider.init.sh
+++ collider.init.sh
@@ -2,6 +2,12 @@
 
 USER=collider
 
+do_start(){
+    echo -n "Starting collider..."
+    sleep 1s
+    echo "ok"
+    echo "The answer is 42. Please, come back again after 1 billion years."
+
 case $1 in
         *)
                 echo Uknown Action: $1
